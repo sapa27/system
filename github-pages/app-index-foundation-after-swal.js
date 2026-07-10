e4d623a56b583268fef3232a395b5cd820a4dbbe
sapa27 <natthapong.sripa@gmail.com>
2026-07-10 20:32:12 +0700

    Add files via upload

--- github-pages/app-index-foundation-after-swal.js
+++ github-pages/app-index-foundation-after-swal.js
@@ -214,8 +214,8 @@ window.__SAFE_LOGO_URL__ = window.currentLogoUrl || window.DEFAULT_LOGO, (functi
   runtimeAuthContract: "runtime-auth-production-current-overwrite-correction",
   logoUrl: "data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%22128%22%20height%3D%22128%22%20viewBox%3D%220%200%20128%20128%22%3E%3Crect%20width%3D%22128%22%20height%3D%22128%22%20rx%3D%2224%22%20fill%3D%22%23f8fafc%22/%3E%3Ccircle%20cx%3D%2264%22%20cy%3D%2248%22%20r%3D%2226%22%20fill%3D%22%23d4af37%22/%3E%3Cpath%20d%3D%22M28%20100h72M40%2088h48M48%2074h32%22%20stroke%3D%22%23334155%22%20stroke-width%3D%227%22%20stroke-linecap%3D%22round%22/%3E%3Ctext%20x%3D%2264%22%20y%3D%2255%22%20text-anchor%3D%22middle%22%20font-family%3D%22Sarabun%2C%20Arial%22%20font-size%3D%2218%22%20fill%3D%22%23334155%22%3E%E0%B8%AA%E0%B8%A0%E0%B8%B2%3C/text%3E%3C/svg%3E",
   defaultRoute: "/dashboard",
-  appStamp: "github-pages-static-commission-v1.2-gas-hosted-production-2026-07-10-r27",
-  assetStamp: "asset-manifest-commission-v1.2-gas-hosted-production-2026-07-10-r27",
+  appStamp: "github-pages-static-commission-v1.2-gas-hosted-production-2026-07-10-r28",
+  assetStamp: "asset-manifest-commission-v1.2-gas-hosted-production-2026-07-10-r28",
   baseUrl: "",
   uiMode: "vue3",
   enabledVuePages: ["/login",
@@ -233,7 +233,7 @@ window.__SAFE_LOGO_URL__ = window.currentLogoUrl || window.DEFAULT_LOGO, (functi
   printStandard: {
   }
 }, window.__APP_ASSET_MANIFEST__ = {
-  stamp: "asset-manifest-commission-v1.2-gas-hosted-production-2026-07-10-r27",
+  stamp: "asset-manifest-commission-v1.2-gas-hosted-production-2026-07-10-r28",
   bundles: {
     appCritical: {
       files: []
