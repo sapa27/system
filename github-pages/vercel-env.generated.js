2a7c6718de82d5a1dd6e948dbc356aa80fafa7e5
sapa27 <natthapong.sripa@gmail.com>
2026-07-13 08:39:06 +0700

    Add files via upload

--- github-pages/vercel-env.generated.js
+++ github-pages/vercel-env.generated.js
@@ -5,8 +5,8 @@
   var cfg = {
     "releaseTrack": "production-current",
     "version": "1.2.0-production-current",
-    "releaseStamp": "commission-v1.2-gas-hosted-production-2026-07-11-r40",
-    "assetStamp": "asset-manifest-commission-v1.2-gas-hosted-production-2026-07-11-r40",
+    "releaseStamp": "commission-v1.2-gas-hosted-production-2026-07-13-r76",
+    "assetStamp": "asset-manifest-commission-v1.2-gas-hosted-production-2026-07-13-r76",
     "hostingTarget": "vercel-api-proxy",
     "vercelStaticFrontendReady": true,
     "vercelApiProxyEnabled": true,
